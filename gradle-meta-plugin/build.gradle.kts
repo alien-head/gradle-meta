54ff84e96ff355335fbe7fd795995516050fda93
Christopher Poulsen <jlpoulsen12@gmail.com>
2022-01-17 19:33:55 -0600

    Add shell gradle plugin project.

--- gradle-meta-plugin/build.gradle.kts
+++ gradle-meta-plugin/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-  kotlin("jvm") version "1.6.10" apply false
-
+  kotlin("jvm")
 }
 
 repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "gradle-meta"
 
 include("gradle-meta-core")
+include("gradle-meta-plugin")
